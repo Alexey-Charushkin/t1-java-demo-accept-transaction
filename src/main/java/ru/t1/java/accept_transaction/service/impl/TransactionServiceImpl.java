2035fd0395c96ee3024d2f0d328efd41fae1599a
Alexey Charushkin <charushkin.lexa@yandex.ru>
2025-02-07 10:52:33 +0300

    refactor: final refactor

--- src/main/java/ru/t1/java/accept_transaction/service/impl/TransactionServiceImpl.java
+++ src/main/java/ru/t1/java/accept_transaction/service/impl/TransactionServiceImpl.java
@@ -18,10 +18,7 @@ import ru.t1.java.accept_transaction.model.dto.TransactionRequest;
 import ru.t1.java.accept_transaction.model.dto.TransactionResponse;
 import ru.t1.java.accept_transaction.service.TransactionService;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -40,24 +37,32 @@ public class TransactionServiceImpl implements TransactionService {
     @Value("${t1.limits.transaction_timeframe}")
     private long transactionTimeframe;
     private final KafkaProducer producer;
-    private final Map<String, List<Long>> transactionCounts = new ConcurrentHashMap<>();
+    private final Map<String, List<TransactionRequest>> transactionBuffer = new ConcurrentHashMap<>();
+
+    //  private final List<TransactionResponse> responseBuffer = Collections.synchronizedList(new ArrayList<>());
+
 
     @Override
     public void checkTransactions(List<TransactionRequest> messageList) {
+        List<TransactionResponse> checkedTransactions = new ArrayList<>();
+
 
         for (TransactionRequest transactionRequest : messageList) {
             log.info("Получена транзакция: " + transactionRequest);
 
+            // получили составной ключ
             String key = transactionRequest.getClientId().toString() + "_" + transactionRequest.getAccountId().toString();
-
+            // получили текушее время
             long currentTimestamp = System.currentTimeMillis();
 
-            transactionCounts.computeIfPresent(key, (k, timestamps) -> {
-                timestamps.removeIf(timestamp -> currentTimestamp - timestamp > transactionTimeframe);
-                return timestamps;
+            // удаляем устаревшие записи
+            transactionBuffer.computeIfPresent(key, (k, transactions) -> {
+                transactions.removeIf(transaction -> currentTimestamp - transaction.getTimestamp().getTime() > transactionTimeframe);
+                return transactions;
             });
 
-            transactionCounts.computeIfAbsent(key, k -> new ArrayList<>()).add(currentTimestamp);
+            // Добавляем новую транзакцию
+            transactionBuffer.computeIfAbsent(key, k -> new ArrayList<>()).add(transactionRequest);
 
             TransactionResponse transactionResponse = TransactionResponse.builder()
                     .accountId(transactionRequest.getAccountId())
@@ -66,22 +71,44 @@ public class TransactionServiceImpl implements TransactionService {
 
             if (transactionRequest.getTransactionAmount().compareTo(transactionRequest.getAccountBalance()) > 0) {
                 transactionResponse.setState(TransactionState.REJECTED);
-                sendTransactionResponse(topic, transactionResponse);
+                //   sendTransactionResponse(topic, transactionResponse);
                 continue;
             }
-            if (transactionCounts.get(key).size() > transactionLimit) {
-                transactionResponse.setState(TransactionState.BLOCKED);
+            if (transactionBuffer.get(key).size() > transactionLimit) {
+               // transactionResponse.setState(TransactionState.BLOCKED);
+                blockedTransaction(key, checkedTransactions);
             } else transactionResponse.setState(TransactionState.ACCEPTED);
 
             log.error("Проверенные транзакции: " + transactionResponse);
-            sendTransactionResponse(topic, transactionResponse);
+            checkedTransactions.add(transactionResponse);
+            //  sendTransactionResponse(topic, transactionResponse);
         }
-
+        sendTransactionResponse(topic, checkedTransactions);
     }
 
-    private void sendTransactionResponse(String topic, TransactionResponse transaction) {
+    private void blockedTransaction(String key, List<TransactionResponse> responses) {
 
-        Message<TransactionResponse> message = MessageBuilder.withPayload(transaction)
+        // Получили список транзакций для блокировки
+        List<TransactionRequest> requests = transactionBuffer.get(key);
+
+        // Создаем HashMap для быстрого поиска TransactionRequest по transactionId
+        Map<UUID, TransactionRequest> requestMap = new HashMap<>();
+        for (TransactionRequest request : requests) {
+            requestMap.put(request.getTransactionId(), request);
+        }
+
+        // Обновляем состояние транзакций
+        for (TransactionResponse response : responses) {
+            TransactionRequest request = requestMap.get(response.getTransactionId());
+            if (request != null) {
+                response.setState(TransactionState.BLOCKED);
+            }
+        }
+    }
+
+    private void sendTransactionResponse(String topic, List<TransactionResponse> transactions) {
+
+        Message<List<TransactionResponse>> message = MessageBuilder.withPayload(transactions)
                 .setHeader(KafkaHeaders.TOPIC, topic)
                 .setHeader(KafkaHeaders.KEY, UUID.randomUUID().toString())
                 .build();
@@ -98,4 +125,25 @@ public class TransactionServiceImpl implements TransactionService {
         });
         future.join();
     }
+
+    // старый вариант
+//    private void sendTransactionResponse(String topic, TransactionResponse transaction) {
+//
+//        Message<TransactionResponse> message = MessageBuilder.withPayload(transaction)
+//                .setHeader(KafkaHeaders.TOPIC, topic)
+//                .setHeader(KafkaHeaders.KEY, UUID.randomUUID().toString())
+//                .build();
+//
+//        CompletableFuture<SendResult<Object, Object>> future = producer.sendMessage(message);
+//        future.thenAccept(sendResult -> {
+//            log.info("Transaction sent successfully to topic: {}", sendResult.getRecordMetadata().topic());
+//            ProducerRecord<Object, Object> record = sendResult.getProducerRecord();
+//            log.info("Message key: {}", record.key());
+//            log.info("Message value: {}", record.value());
+//        }).exceptionally(ex -> {
+//            log.error("Failed to send transaction: {}", ex.getMessage(), ex);
+//            throw new RuntimeException("Failed to send transaction", ex);
+//        });
+//        future.join();
+//    }
 }
